e248f366f93f8d85cccd4ddf19dca27b06d8a99a
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-30 12:26:18 -0300

    setting up react-app

---
+++ client/src/Fib.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Fib extends Component {
+    state = {
+        seenIndexes: [],
+        values: {},
+        indexes: ""
+    };
+    componentDidMount() {
+        this.fetchValues();
+        this.fetchIndexes();
+    }
+
+    async fetchValues() {
+        const values = await axios.get('/api/values/current');
+        this.setState({ values: values.data });
+    }
+
+    async fetchIndexes() {
+        const seenIndexes = await axios.get('/api/values/all');
+        this.setState({
+            seenIndexes: seenIndexes.data
+        });
+    }
+
+    renderSeenIndexes(){
+        return this.state.seenIndexes.map(({number})=>number).join(', ');
+    }
+
+    renderValues(){
+        const entries = [];
+
+        for(let key in this.state.values){
+            entries.push(
+                <div key={key}>
+                    For index {key} I Calculated{this.state.values[key]}
+                </div>
+            );
+        }
+        return entries;
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label>Enter your index: </label>
+                    <input
+                    value={this.state.index}
+                    onChange={event=>this.setState({index: event.target.value})}
+                    ></input>
+                    <button>Submit</button>
+                </form>
+                <h3>Indexes i have seen:</h3>
+                {this.renderSeenIndexes()}
+                <h3>Calculated Values:</h3>
+                {this.renderValues()}
+
+            </div>
+        )
+    }
+};
--- server/index.js
+++ server/index.js
@@ -20,10 +20,51 @@ const pgClient = new Pool({
 
 });
 
-pdClient.on('error', () => console.log('Lost P conn'))
+pgClient.on('error', () => console.log('Lost P conn'))
 
 pgClient.on("connect", (client) => {
     client
         .query("CREATE TABLE IF NOT EXISTS values (number INT)")
         .catch((err) => console.error(err));
+});
+
+//redis cliente setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host:keys.redisHost,
+    port:keys.redisPort,
+    retry_strategy:()=>1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get('/',(req,res)=>{
+    res.send('Hi');
+});
+app.get('/values/all',async(req,res)=>{
+    const values = await pgClient.query('SELECT * from values');
+    res.send(values.rows);
+});
+app.get('/values/current',async(req,res)=>{
+    redis.Cliente.hgetall('values',(err,values)=>{
+        res.send(values);
+    });
+});
+
+app.post('/values',async(req,res)=>{
+    const index = req.body.index;
+
+    if(parseInt(index)>40){
+        return res.status(422).send('index too high');
+    }
+
+    redisCliente.hset('values',index,'Nothing yet!');
+    redisPublisher.publish('insert',index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)',[index]);
+    res.send({workind:true});
+});
+
+app.listen(5000,err=>{
+    console.log('Listening...');
 });
