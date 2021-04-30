748b114922fe2951cd301cdc4cc5034e94dfa7db
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-30 11:03:58 -0300

    express db keys config

---
+++ server/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPost: process.env.REDIS_PORT,
+    pgUser: process.env.PGSER,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+
+};
